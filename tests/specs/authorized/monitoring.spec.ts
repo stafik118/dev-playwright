a8f75671adb97af027cef1cd53d298a05513decf
stafik118 <staf118@mail.ru>
2025-09-04 22:52:21 +0300

    Мониторинг. Артикулы с продажами

--- tests/specs/authorized/monitoring.spec.ts
+++ tests/specs/authorized/monitoring.spec.ts
@@ -1,8 +1,22 @@
-import test from '@playwright/test';
+import test, { expect } from '@playwright/test';
 import { Monitoring } from '../../pages/Monitoring';
 
 test('Открытие мониторинга', async ({ page }) => {
   const monitoring = new Monitoring(page);
   await monitoring.open();
   await page.getByRole('cell', { name: 'Основные' }).waitFor({ state: 'visible', timeout: 5000 });
+});
+test('Проверка корректности колонки Артикулов с продажами', async ({ page }) => {
+  const monitoring = new Monitoring(page);
+  await monitoring.open();
+  await page.getByText('Артикулов с продажами, %').click();
+  await page.getByText('Артикулов с продажами, %').click();
+  const cell = page.locator('.ant-table-row > td:nth-child(36)').first();
+  await expect(cell).toBeVisible();
+  const text = await cell.textContent();
+  if (!text) {
+    throw new Error('Текст в ячейке отсутствует или пуст');
+  }
+  const value = parseFloat(text.trim());
+  await expect(value).toBeLessThanOrEqual(100);
 });
